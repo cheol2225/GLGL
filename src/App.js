5413aa7fb43a5be2ea1ca881581dff1964bcb9f9
철이월드 <cheol2225@gmail.com>
2023-02-14 22:46:16 +0900

    로그인 화면

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { AssertionError } from 'assert';
 import React, {useState} from 'react';
 import {StatusBar, Image} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
 import {theme} from './theme';
 
+const cacheImages = images => {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Assert.fromModule(image).downloadAsync();
+    }
+  });
+};
+
 const App = () => {
-  return (
+  const [isReady, setIsReady] = useState(false);
+
+  const _loadAssets = async () => {
+    const imageAssets = cacheImages([require('../assets/splash.png')]);
+    const fontAssets = cacheFonts([]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  };
+
+  return isReady ? (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="dark-content" />
     </ThemeProvider>
+  ) : (
+    // eslint-disable-next-line react/jsx-no-undef
+    <AppLoading
+      startAsync={_loadAssets}
+      onFinish={() => setIsReady(true)}
+      onError={console.warn}
+    />
   );
 };
 
